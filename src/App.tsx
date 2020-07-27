05338ac0bbdcdae33a0c25fc7825adb1f825e72a
BOUHOU ABDELILAH <bouhoudev18@gmail.com>
2020-07-27 01:51:24 +0100

    fix configuration for eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>hy bouhou</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
